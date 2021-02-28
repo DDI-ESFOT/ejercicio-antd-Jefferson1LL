81988fdfff68ef94beebf137d7bb6f63e3eea8d7
Jefferson1LL <jefferson.rllumiquinga93@gmail.com>
2021-02-28 12:31:29 -0500

    Movies

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import logo from '../images/logo.svg';
+import '../styles/App.css';
+import Movies from "./Movies";
+
+function App() {
+  return (
+    <>
+      <Movies/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CommentList.js
@@ -0,0 +1,8 @@
+import { Comment, Avatar, Form, Button, List, Input } from 'antd';
+import moment from 'moment';
+
+const { TextArea } = Input;
+
+const CommentList = ({ comments }) =>{
+
+}
